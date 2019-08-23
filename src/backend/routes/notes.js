45ed2e352a77ce58817a78c93f81fe1d3b32ecca
wensug <wensug@gmail.com>
2019-08-24 00:42:33 +0100

    Finish and testing API endpoints

--- src/backend/routes/notes.js
+++ src/backend/routes/notes.js
@@ -1,28 +1,60 @@
 const router = require('express').Router();
 let Note = require('../models/notes.model');
 
+// Defined get data route
 router.route('/').get((req, res) => {
   Note.find()
     .then(notes => res.json(notes))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+// Defined store route
 router.route('/add').post((req, res) => {
   const title = req.body.title;
   const text = req.body.text;
-  const status = req.body.status;
   const created = Date.parse(req.body.created);
+  const status = req.body.status;
 
   const newNote = new Note({
     title,
     text,
-    status,
     created,
+    status,
   });
 
   newNote.save()
   .then(() => res.json('Note added!'))
   .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Defined edit route
+router.route('/:id').get((req, res) => {
+  Note.findById(req.params.id)
+    .then(note => res.json(note))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Defined delete route
+router.route('/:id').delete((req, res) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Note deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Defined update route
+router.route('/update/:id').post((req, res) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      note.title = req.body.title;
+      note.text = req.body.text;
+      note.created = Date.parse(req.body.created);
+      note.status = req.body.status;
+
+      note.save()
+        .then(() => res.json('Note updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
